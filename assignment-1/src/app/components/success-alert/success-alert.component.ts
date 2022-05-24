6284169703b1adcfd47598ad6912888be1e3282e
onkar <onkar21.rai@gmail.com>
2022-05-25 00:29:25 +0530

    styled the components and aligned them beneath each other

--- assignment-1/src/app/components/success-alert/success-alert.component.ts
+++ assignment-1/src/app/components/success-alert/success-alert.component.ts
@@ -6,5 +6,8 @@ import { Component } from "@angular/core";
     styleUrls: ['./success-alert.component.css']
 })
 export class SuccessAlertComponent {
+    constructor() { }
 
+    ngOnInit(): void {
+    }
 }
